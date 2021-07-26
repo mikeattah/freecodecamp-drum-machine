cf3eb978388aa5ea906c4e950dcfa601309c9dd6
Mike Attah <mailenemona@gmail.com>
2021-07-26 15:38:16 +0100

    updated user interface for App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div id="drum-machine">
+          <div className="fcc-logo">
+            <p>
+              FCC <i class="fa fa-free-code-camp" aria-hidden="true"></i>
+            </p>
+          </div>
+          <div id="display">
+            <div className="drum-pad-container">
+              <button className="drum-pad">
+                <audio id="Q" className="clip" src=""></audio>Q
+              </button>
+              <button className="drum-pad">
+                <audio id="W" className="clip" src=""></audio>W
+              </button>
+              <button className="drum-pad">
+                <audio id="E" className="clip" src=""></audio>E
+              </button>
+              <button className="drum-pad">
+                <audio id="A" className="clip" src=""></audio>A
+              </button>
+              <button className="drum-pad">
+                <audio id="S" className="clip" src=""></audio>S
+              </button>
+              <button className="drum-pad">
+                <audio id="D" className="clip" src=""></audio>D
+              </button>
+              <button className="drum-pad">
+                <audio id="Z" className="clip" src=""></audio>Z
+              </button>
+              <button className="drum-pad">
+                <audio id="X" className="clip" src=""></audio>X
+              </button>
+              <button className="drum-pad">
+                <audio id="C" className="clip" src=""></audio>C
+              </button>
+            </div>
+            <div className="controls-container">
+              <div className="power-control">
+                <p className="text">Power</p>
+                <div className="power-toggle">
+                  <div id="power-toggle-on"></div>
+                  <div id="power-toggle-off"></div>
+                </div>
+              </div>
+              <div className="text-display">{}</div>
+              <input type="range" className="volume-control"></input>
+              <div className="bank-control">
+                <p className="text">Bank</p>
+                <div className="bank-toggle">
+                  <div id="bank-toggle-on"></div>
+                  <div id="bank-toggle-off"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
